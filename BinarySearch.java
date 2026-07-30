5cc623a2ce709590289c32a71161418d1d3b07b2
Priyesh Choudhary <143314183+priyeshkr18@users.noreply.github.com>
2026-07-30 12:00:51 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+	public static void main(String[] args) {
+
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
+		int target = 40;
+
+		int low = 0;
+		int high = arr.length - 1;
+		int index = -1;
+
+		while (low <= high) {
+
+			int mid = (low + high) / 2;
+
+			if (arr[mid] == target) {
+				index = mid;
+				break;
+			} else if (arr[mid] < target) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+
+		if (index != -1)
+			System.out.println("Found at index " + index);
+		else
+			System.out.println("Not Found");
+	}
+}
